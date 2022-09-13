a3340d964bd4ffd5c8d4196ed66bb3ea6a4257ff
minjong kim <alswhddkdl@gmail.com>
2022-09-13 23:37:29 +0900

    FEAT: 계산기 UI 구성하기

--- basic/Calculator/app/src/main/java/com/fc/calculator/MainActivity.kt
+++ basic/Calculator/app/src/main/java/com/fc/calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.fc.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun buttonClicked(v: View){
+
+    }
+    private fun historyButtonClicked(v: View){
+
+    }
+    private fun equalButtonClicked(v: View){
+
+    }
+    private fun resultButtonClicked(v: View){
+
+    }
+    private fun clearButtonClicked(v: View){
+
+    }
+
+
 }
