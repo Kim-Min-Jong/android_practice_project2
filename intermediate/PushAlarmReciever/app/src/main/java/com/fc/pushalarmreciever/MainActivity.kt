3f0e9b7287e2c84c1b1452630e6fad084bd6cad8
minjong kim <alswhddkdl@gmail.com>
2022-09-18 22:10:59 +0900

    FEAT: 기본 UI 구성하기

--- intermediate/PushAlarmReciever/app/src/main/java/com/fc/pushalarmreciever/MainActivity.kt
+++ intermediate/PushAlarmReciever/app/src/main/java/com/fc/pushalarmreciever/MainActivity.kt
@@ -2,8 +2,16 @@ package com.fc.pushalarmreciever
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val resultTv: TextView by lazy{
+        findViewById<TextView>(R.id.resultTv)
+    }
+    private val tokenTv: TextView by lazy{
+        findViewById<TextView>(R.id.tokenTv)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
