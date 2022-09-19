c5e8ace07dd852808109b7d7c0273522babd2acb
minjong kim <alswhddkdl@gmail.com>
2022-09-19 16:40:10 +0900

    FEAT: Cloud Messaging 구성 및 테스트

--- intermediate/PushAlarmReciever/app/src/main/java/com/fc/pushalarmreciever/MainActivity.kt
+++ intermediate/PushAlarmReciever/app/src/main/java/com/fc/pushalarmreciever/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fc.pushalarmreciever
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val resultTv: TextView by lazy{
@@ -15,5 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+    private fun initFirebase(){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener{
+            if(it.isSuccessful){
+                tokenTv.text = it.result
+            }
+        }
     }
 }
