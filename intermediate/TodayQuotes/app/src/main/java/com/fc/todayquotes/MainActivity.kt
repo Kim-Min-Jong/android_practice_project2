7c46492196297d91115d1aa180c81578266ce05f
minjong kim <alswhddkdl@gmail.com>
2022-09-20 21:52:06 +0900

    FEAT: remote config 연동하기

--- intermediate/TodayQuotes/app/src/main/java/com/fc/todayquotes/MainActivity.kt
+++ intermediate/TodayQuotes/app/src/main/java/com/fc/todayquotes/MainActivity.kt
@@ -3,6 +3,11 @@ package com.fc.todayquotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private val viewPager: ViewPager2 by lazy {
@@ -13,9 +18,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+        initData()
     }
 
     private fun initViews() {
-        viewPager.adapter = QuotesPagerAdapter(emptyList())
+
+    }
+    private fun initData(){
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if(it.isSuccessful){
+                val quote = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quote, isNameRevealed)
+
+            }
+        }
+    }
+
+    private fun displayQuotesPager(quote: List<Quote>, nameRevealed: Boolean) {
+        viewPager.adapter = QuotesPagerAdapter(quote, nameRevealed)
+    }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+        for(i in 0 until jsonArray.length()){
+            val jsonObj = jsonArray.getJSONObject(i)
+            jsonObj?.let{
+                jsonList = jsonList + it
+            }
+        }
+
+        return jsonList.map{
+            Quote(it.getString("quote"), it.getString("name"))
+        }
     }
 }
--- intermediate/TodayQuotes/app/src/main/java/com/fc/todayquotes/QuotesPagerAdapter.kt
+++ intermediate/TodayQuotes/app/src/main/java/com/fc/todayquotes/QuotesPagerAdapter.kt
@@ -7,8 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class QuotesPagerAdapter(
-    private val quotes: List<Quote>
-): RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
+    private val quotes: List<Quote>,
+    private val isNameRevealed: Boolean,
+) : RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder =
         QuoteViewHolder(
@@ -16,20 +17,25 @@ class QuotesPagerAdapter(
         )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position])
+        holder.bind(quotes[position], isNameRevealed)
     }
 
     override fun getItemCount(): Int {
         return quotes.size
     }
 
-    inner class QuoteViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    inner class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val quoteTv: TextView = itemView.findViewById(R.id.quoteTv)
         private val nameTv: TextView = itemView.findViewById(R.id.nameTv)
 
-        fun bind(quote: Quote){
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
             quoteTv.text = quote.quote
-            nameTv.text = quote.name
+            if (isNameRevealed) {
+                nameTv.text = quote.name
+                nameTv.visibility = View.VISIBLE
+            } else{
+                nameTv.visibility = View.GONE
+            }
         }
 
     }
