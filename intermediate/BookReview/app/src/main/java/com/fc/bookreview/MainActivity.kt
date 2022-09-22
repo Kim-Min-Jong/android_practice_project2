eca617615fe7dcb5d84a115652eeff54a80262e8
minjong kim <alswhddkdl@gmail.com>
2022-09-22 22:31:04 +0900

    FEAT: Retrofit, Naver API를 이용하여 책 정보 가져오기

--- intermediate/BookReview/app/src/main/java/com/fc/bookreview/MainActivity.kt
+++ intermediate/BookReview/app/src/main/java/com/fc/bookreview/MainActivity.kt
@@ -2,10 +2,65 @@ package com.fc.bookreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.fc.bookreview.api.BookService
+import com.fc.bookreview.model.SearchBookDto
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://openapi.naver.com/ ")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+        val keys = getJsonObject()
+        bookService.getBooksByName(keys.getString(CLIENT_ID), keys.getString(CLIENT_SECRET), "개미")
+            .enqueue(object: Callback<SearchBookDto>{
+                override fun onResponse(
+                    call: Call<SearchBookDto>,
+                    response: Response<SearchBookDto>
+                ) {
+                    if(response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT SUCCESS")
+                        return
+                    }
+                    response.body()?.let{
+                        Log.d(TAG, it.toString())
+                        it.books.forEach { book ->
+                            Log.d(TAG, book.toString())
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                }
+
+            })
+    }
+    private fun getJsonObject(): JSONObject{
+        val jsonString = assets.open(KEY).reader().readText()
+        val jsonArray = JSONArray(jsonString)
+        println(jsonString)
+        println(jsonArray)
+        return jsonArray.getJSONObject(0)
+    }
+
+    companion object{
+        private const val KEY = "key.json"
+        private const val CLIENT_ID = "client_id"
+        private const val CLIENT_SECRET = "client_secret"
+        private const val TAG = "MainActivity"
     }
 }
---
+++ intermediate/BookReview/app/src/main/java/com/fc/bookreview/api/BookService.kt
@@ -0,0 +1,17 @@
+package com.fc.bookreview.api
+
+import com.fc.bookreview.model.SearchBookDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface BookService {
+    @GET("/v1/search/book.json")
+    fun getBooksByName(
+        @Header("X-Naver-Client-Id") id: String,
+        @Header("X-Naver-Client-Secret") secretKey: String,
+        @Query("query") keyword: String
+    ): Call<SearchBookDto>
+
+}
