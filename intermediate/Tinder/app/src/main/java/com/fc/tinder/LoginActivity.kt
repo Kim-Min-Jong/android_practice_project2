b7b80ab7f5a91f9aa813da389bc0a80b349f4afc
minjong kim <alswhddkdl@gmail.com>
2022-09-25 23:16:08 +0900

    FEAT: Facebook 로그인 구현(deprecated 된 메소드 대응 필요- 추후 대응 예정)

--- intermediate/Tinder/app/src/main/java/com/fc/tinder/LoginActivity.kt
+++ intermediate/Tinder/app/src/main/java/com/fc/tinder/LoginActivity.kt
@@ -1,27 +1,47 @@
 package com.fc.tinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.fc.tinder.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
+
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
     private var binding: ActivityLoginBinding? = null
+
+//    private var result = registerForActivityResult(
+//        ActivityResultContracts.StartActivityForResult()){ result ->
+//            if(result.resultCode == RESULT_OK){
+//                    callbackManager.
+//            }
+//        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         auth = Firebase.auth
-
+        callbackManager = CallbackManager.Factory.create()
         initLoginButton()
         initSignUpButton()
         initEmailAndPasswordEditText()
+        initFacebookLoginButton()
     }
 
     private fun initLoginButton() {
@@ -76,8 +96,41 @@ class LoginActivity : AppCompatActivity() {
             binding?.signUpButton?.isEnabled = enable
         }
     }
+    private fun initFacebookLoginButton(){
+        binding?.facebookLoginButton?.setPermissions("email", "public_profile")
+        binding?.facebookLoginButton?.registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult) {
+                // 성공 시 콜백
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) {
+                        if(it.isSuccessful){
+                            finish()
+                        } else{
+                            Toast.makeText(this@LoginActivity,"페이스북 로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+            override fun onCancel() {
+                // 로그인 하다가 취소했을 때 실행되는 콜백
+            }
+
+            override fun onError(error: FacebookException) {
+                //에러시 콜백
+                Toast.makeText(this@LoginActivity,"페이스북 로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
