48c099e116192f0ab757603e72bac43cb1df7c03
minjong kim <alswhddkdl@gmail.com>
2022-09-29 11:36:11 +0900

    FEAT: naver map api 세팅

--- intermediate/Airbnb/app/src/main/java/com/fc/airbnb/MainActivity.kt
+++ intermediate/Airbnb/app/src/main/java/com/fc/airbnb/MainActivity.kt
@@ -2,10 +2,53 @@ package com.fc.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fc.airbnb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // 맵뷰에 생명주기 연결결
+       binding?.mapView?.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding?.mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding?.mapView?.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding?.mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding?.mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding?.mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+        binding?.mapView?.onDestroy()
     }
 }
