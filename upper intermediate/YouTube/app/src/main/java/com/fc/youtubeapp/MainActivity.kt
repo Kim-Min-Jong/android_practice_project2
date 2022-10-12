40e2029b637cc6ac9bc6dc8c19ba3753db1eb0c1
minjong kim <alswhddkdl@gmail.com>
2022-10-12 14:49:56 +0900

    FEAT: Motion Layout 상세설정(transition, animation을 constraint로 지정)

--- upper intermediate/YouTube/app/src/main/java/com/fc/youtubeapp/MainActivity.kt
+++ upper intermediate/YouTube/app/src/main/java/com/fc/youtubeapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.fc.youtubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fc.youtubeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment())
+            .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
