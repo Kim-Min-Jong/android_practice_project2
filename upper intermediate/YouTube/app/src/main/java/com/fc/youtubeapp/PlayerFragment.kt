16dd881a95915254c21102f1d4007fd2517c0c12
minjong kim <alswhddkdl@gmail.com>
2022-10-13 12:48:57 +0900

    FEAT: ExoPlayer를 이용하여 동영상 재생하기(Control)

--- upper intermediate/YouTube/app/src/main/java/com/fc/youtubeapp/PlayerFragment.kt
+++ upper intermediate/YouTube/app/src/main/java/com/fc/youtubeapp/PlayerFragment.kt
@@ -1,5 +1,6 @@
 package com.fc.youtubeapp
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,6 +11,11 @@ import com.fc.youtubeapp.adapter.VideoAdapter
 import com.fc.youtubeapp.databinding.FragmentPlayerBinding
 import com.fc.youtubeapp.dto.VideoDto
 import com.fc.youtubeapp.service.VideoService
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +26,7 @@ import kotlin.math.abs
 class PlayerFragment : Fragment(R.layout.fragment_player) {
     private var binding: FragmentPlayerBinding? = null
     private lateinit var videoAdapter: VideoAdapter
+    private var player: ExoPlayer? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,16 +34,48 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
         initMotionLayoutEvent()
         initRecyclerView()
-
+        initPlayer()
+        initControlButton()
         getVideoList()
+    }
+    private fun initControlButton() {
+        binding?.bottomPlayerControlBtn?.setOnClickListener {
+            val player = this.player ?: return@setOnClickListener
+
+            if(player.isPlaying){
+                player.pause()
+            } else {
+                player.play()
+            }
+        }
+    }
+    private fun initPlayer() {
+        context?.let {
+            player = ExoPlayer.Builder(it).build()
+        }
+
+        binding?.playerView?.player = player
+        binding?.let{
+            player?.addListener(object: Player.Listener{
+                // 플레이 여부가 바뀔 때 마다 실행되는 콜백  isPlaying, true - 실행  false - 멈춤
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+                    if(isPlaying){
+                        it.bottomPlayerControlBtn.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                    } else {
+                        it.bottomPlayerControlBtn.setImageResource(R.drawable.ic_baseline_pause_24)
+                    }
+                }
+            })
+        }
     }
 
     private fun initRecyclerView() {
-        videoAdapter = VideoAdapter{ url, title ->
-            play(url,title)
+        videoAdapter = VideoAdapter { url, title ->
+            play(url, title)
         }
 
-        binding?.fragmentRecyclerView?.apply{
+        binding?.fragmentRecyclerView?.apply {
             adapter = videoAdapter
             layoutManager = LinearLayoutManager(context)
         }
@@ -78,27 +117,28 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
         })
     }
+
     private fun getVideoList() {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://run.mocky.io/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        retrofit.create(VideoService::class.java).also{
-            it.listVideos().enqueue(object: Callback<VideoDto> {
+        retrofit.create(VideoService::class.java).also {
+            it.listVideos().enqueue(object : Callback<VideoDto> {
                 override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
-                    if(response.isSuccessful.not()){
-                        Log.d("MainActivity"," Response fail1")
+                    if (response.isSuccessful.not()) {
+                        Log.d("MainActivity", " Response fail1")
                         return
                     }
-                    response.body()?.let{ videoDto ->
-                        Log.d("MainActivity"," Response success")
+                    response.body()?.let { videoDto ->
+                        Log.d("MainActivity", " Response success")
                         videoAdapter.submitList(videoDto.videos)
                     }
                 }
 
                 override fun onFailure(call: Call<VideoDto>, t: Throwable) {
-                    Log.d("MainActivity"," Response fail2")
+                    Log.d("MainActivity", " Response fail2")
 
                 }
 
@@ -106,16 +146,31 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         }
     }
 
-    fun play(url:String, title:String) {
-        binding?.let{
+    fun play(url: String, title: String) {
+        context?.let {
+            val dataSourceFactory = DefaultDataSource.Factory(it)
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
+            player?.setMediaSource(mediaSource)
+            player?.prepare()
+            player?.play()
+        }
+
+        binding?.let {
             // 모션레이아웃을 end상태로 트랜지션함
             it.playerMotionLayout.transitionToEnd()
             it.bottomTitleTextView.text = title
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
+        player?.release()
     }
 }
