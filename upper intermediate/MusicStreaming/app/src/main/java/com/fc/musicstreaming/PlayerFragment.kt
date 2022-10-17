b9ff4f1f48ce2f725f590ebf97dbebd2b5c356ee
minjong kim <alswhddkdl@gmail.com>
2022-10-17 21:57:19 +0900

    FEAT: 재생화면 UI 구성하기

---
+++ upper intermediate/MusicStreaming/app/src/main/java/com/fc/musicstreaming/PlayerFragment.kt
@@ -0,0 +1,20 @@
+package com.fc.musicstreaming
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() : PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+}
