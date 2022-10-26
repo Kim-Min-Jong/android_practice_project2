bd54c0c21eb5380a1c51fb461a1e780b01314bf0
minjong kim <alswhddkdl@gmail.com>
2022-10-26 14:58:02 +0900

    init OTTIntroClone app

---
+++ upper intermediate/OTTIntroClone/app/src/main/java/com/fc/ottintroclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fc.ottintroclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
