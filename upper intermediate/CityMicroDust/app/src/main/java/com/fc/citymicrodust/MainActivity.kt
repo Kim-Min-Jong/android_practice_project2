1ec8ce99fc5b52c60aa7005caeeeaab75f2bffa2
minjong kim <alswhddkdl@gmail.com>
2022-11-02 21:48:51 +0900

    FEAT: FusedLocationProviderClient로 사용자 위치정보 불러오기

--- upper intermediate/CityMicroDust/app/src/main/java/com/fc/citymicrodust/MainActivity.kt
+++ upper intermediate/CityMicroDust/app/src/main/java/com/fc/citymicrodust/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.fc.citymicrodust
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.fc.citymicrodust.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        initVariables()
+        requestLocationPermissions()
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        cancellationTokenSource = CancellationTokenSource()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if(!locationPermissionGranted){
+            finish()
+        } else {
+            // 실제 위치정보로 측정소 위치 가져오기 todo
+            fusedLocationProviderClient.getCurrentLocation(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener {
+                binding?.tv?.text = it.toString()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+        cancellationTokenSource?.cancel()
+    }
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
