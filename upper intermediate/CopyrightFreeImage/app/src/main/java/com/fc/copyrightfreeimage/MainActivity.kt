400792b7a54703da09ca46bc23a89557d3ed1537
minjong kim <alswhddkdl@gmail.com>
2022-11-09 21:31:25 +0900

    FEAT: 불러온 사진정보를 Recycler View에 바인딩하고, EditText로 사진 검색 기능 구현하기

--- upper intermediate/CopyrightFreeImage/app/src/main/java/com/fc/copyrightfreeimage/MainActivity.kt
+++ upper intermediate/CopyrightFreeImage/app/src/main/java/com/fc/copyrightfreeimage/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.fc.copyrightfreeimage
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fc.copyrightfreeimage.data.Repository
+import com.fc.copyrightfreeimage.data.adapter.PhotoAdapter
 import com.fc.copyrightfreeimage.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -11,17 +17,50 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        initViews()
+        bindViews()
         fetchRandomPhotos()
+    }
+    private fun initViews() {
+        binding?.recyclerView?.apply{
+            layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+            adapter = PhotoAdapter()
+        }
+    }
+
+    private fun bindViews() {
+        binding?.searchEditText?.setOnEditorActionListener { editText, actionId, keyEvent ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let{
+                    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+
+                    it.clearFocus()
+                }
+
+                fetchRandomPhotos(editText.text.toString())
+            }
+            true
+        }
+
+        binding?.refreshLayout?.setOnRefreshListener {
+            fetchRandomPhotos(binding?.searchEditText?.text.toString())
+        }
     }
 
     private fun fetchRandomPhotos(query: String? = null) {
         scope.launch {
             Repository.getRandomPhotos(query)?.let{ photos ->
-                photos
+                (binding?.recyclerView?.adapter as? PhotoAdapter)?.apply {
+                    this.photos = photos
+                    notifyDataSetChanged()
+                }
+                binding?.refreshLayout?.isRefreshing = false
             }
         }
     }
---
+++ upper intermediate/CopyrightFreeImage/app/src/main/java/com/fc/copyrightfreeimage/data/adapter/PhotoAdapter.kt
@@ -0,0 +1,74 @@
+package com.fc.copyrightfreeimage.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.fc.copyrightfreeimage.R
+import com.fc.copyrightfreeimage.data.models.PhotoResponse
+import com.fc.copyrightfreeimage.databinding.ItemPhotoBinding
+
+class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+
+    var photos: List<PhotoResponse> = emptyList()
+
+    inner class ViewHolder(private val binding: ItemPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(photo: PhotoResponse) {
+            // 이미지 조정을 위해 뷰들의 값 불러오기
+            val dimensionRatio = photo.height / photo.width.toFloat()
+            val targetWidth = binding.root.resources.displayMetrics.widthPixels -
+                    (binding.root.paddingStart + binding.root.paddingEnd)
+            val targetHeight = (targetWidth * dimensionRatio).toInt()
+
+            binding.contentContainer.layoutParams =
+                binding.contentContainer.layoutParams.apply {
+                    height = targetHeight
+                }
+
+            Glide.with(binding.root)
+                .load(photo.urls?.regular)
+                .thumbnail(
+                    Glide.with(binding.root)
+                        .load(photo.urls?.thumb)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                )
+                .override(targetWidth, targetHeight)
+                .into(binding.photoImageView)
+
+            Glide.with(binding.root)
+                .load(photo.user?.profileImageUrls?.small)
+                .placeholder(R.drawable.shape_profile_placeholder)
+                .circleCrop()
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(binding.profileImageView)
+
+            if (photo.user?.name.isNullOrBlank()) {
+                binding.authorTextView.visibility = View.GONE
+            } else {
+                binding.authorTextView.visibility = View.VISIBLE
+                binding.authorTextView.text = photo.user?.name
+            }
+
+            if (photo.description.isNullOrBlank()) {
+                binding.descriptionTextView.visibility = View.GONE
+            } else {
+                binding.descriptionTextView.visibility = View.VISIBLE
+                binding.descriptionTextView.text = photo.description
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ItemPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+
+    override fun getItemCount(): Int = photos.size
+}
